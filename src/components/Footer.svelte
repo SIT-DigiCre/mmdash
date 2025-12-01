69a1a7d9222f31a8214987c91226a3de4c22ad0f
newt <ysti2019.win2020.st@gmail.com>
2025-12-01 13:52:24 +0900

    feat: improve footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,19 +1,26 @@
 <footer>
   <small>
-    <a href="https://github.com/SIT-DigiCre/mmdash"
-    target="_blank" rel="noopener noreferrer">GitHub</a>
+    &copy; 2025 <a href="https://x.com/newt239" target="_blank" rel="noopener noreferrer">newt239</a> at <a href="https://digicre.net" target="_blank" rel="noopener noreferrer">DigiCre</a>. Source code is available on <a href="https://github.com/SIT-DigiCre/mmdash" target="_blank" rel="noopener noreferrer">GitHub</a>
   </small>
 </footer>
 
 <style>
   footer {
     max-width: 1000px;
-    margin: 0 auto;
+    margin: 0 auto 1rem;
     padding: 1rem;
+    text-align: end;
   }
 
   small {
     font-size: 0.75rem;
     color: #666;
+    
+    a {
+      color: #000;
+      text-decoration: underline;
+      text-decoration-color: color-mix(in srgb, currentcolor, transparent 40%);
+      text-underline-offset: 0.25em;
+    }
   }
 </style>
